0cd3d68ed5a998d13cf1912099d0c4a3161b751f
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/EasyMacroPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/EasyMacroPlugin/build.pl
+++ lib/Foswiki/Plugins/EasyMacroPlugin/build.pl
@@ -4,5 +4,5 @@ use Foswiki::Contrib::Build;
 
 # Create the build object
 $build = new Foswiki::Contrib::Build('EasyMacroPlugin');
-$build->build($build->{target});
+$build->build( $build->{target} );
 
