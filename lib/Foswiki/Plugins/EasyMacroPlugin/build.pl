17bbebc073859d40d9141a77a9dd3f4404dbafe5
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 09:40:35 +0000

    Item11808: reverting perltidy
    git-svn-id: http://svn.foswiki.org/trunk/EasyMacroPlugin@14708 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/EasyMacroPlugin/build.pl
+++ lib/Foswiki/Plugins/EasyMacroPlugin/build.pl
@@ -3,6 +3,6 @@ BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ); }
 use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new Foswiki::Contrib::Build('EasyMacroPlugin');
-$build->build( $build->{target} );
+my $build = new Foswiki::Contrib::Build('EasyMacroPlugin');
+$build->build($build->{target});
 
